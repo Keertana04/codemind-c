bab9ead6589d8b14f6084b2bafee7cd02a387eaf
Keertana04 <119730054+Keertana04@users.noreply.github.com>
2022-12-16 15:10:00 +0530

    #include<stdio.h> int main() {     int n,r,i=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         if(i<r)         {             i=r;         }         n=n/10;     }     printf("%d",i); }

---
+++ Largest_Digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,i=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        if(i<r)
+        {
+            i=r;
+        }
+        n=n/10;
+    }
+    printf("%d",i);
+}
