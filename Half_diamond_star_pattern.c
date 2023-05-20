e42f4007039277a75247d34d973c8a64437860a4
Keertana04 <119730054+Keertana04@users.noreply.github.com>
2023-05-20 11:05:10 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);      if(n<=2 || n>=100)     {        printf("The pattern is not possible");         return 0;     }     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("*");         }
            printf("
    ");
        }
        for(i=n-1;i>0;i--)
        {
            for(j=1;j<=i;j++)
            {
                printf("*");
            }
           
            printf("
    ");
            
        }
       
     
    }

---
+++ Half_diamond_star_pattern.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+     if(n<=2 || n>=100)
+    {
+       printf("The pattern is not possible");
+        return 0;
+    }
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        
+        printf("
+");
+    }
+    for(i=n-1;i>0;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+       
+        printf("
+");
+        
+    }
+   
+ 
+}
