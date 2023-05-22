b03a3692366c30bdaf6c3898e6f833a5109d3a21
Keertana04 <119730054+Keertana04@users.noreply.github.com>
2023-05-22 20:27:47 +0530

    #include<stdio.h> int main(){     int x,a,b,c,e;     scanf("%d",&x);     a=x%10;     e=x/10;     b=e%10;     c=e/10;     if(a%2==0 && b%2==0 && c%2==0){         printf("Even");     }     else if(a%2==1 && b%2==1 && c%2==1){         printf("Odd");     }     else{         printf("Mixed");     } }

--- Even_Odd_Mixed.c
+++ Even_Odd_Mixed.c
@@ -1,24 +1,18 @@
 #include<stdio.h>
-int main()
-{
-    int n,a;
-    scanf("%d",&n);
-    while(n)
-    {
-        a=n%10;
-        n=n/10;
-        if(a%2==0 && n%2==0)
-        {
-            printf("Even");
-        }
-        else if(a%2!=0 && n%2!=0)
-        {
-            printf("Odd");
-        }
-        else
-        {
-            printf("Mixed");
-        }
-        return 0;
+int main(){
+    int x,a,b,c,e;
+    scanf("%d",&x);
+    a=x%10;
+    e=x/10;
+    b=e%10;
+    c=e/10;
+    if(a%2==0 && b%2==0 && c%2==0){
+        printf("Even");
+    }
+    else if(a%2==1 && b%2==1 && c%2==1){
+        printf("Odd");
+    }
+    else{
+        printf("Mixed");
     }
 }
