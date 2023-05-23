2bd1b53734689c584f1761b5c2721ddab09a8a91
Keertana04 <119730054+Keertana04@users.noreply.github.com>
2023-05-23 11:28:41 +0530

    #include<stdio.h> int main(){     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++){         int x,y,sum;         scanf("%d%d",&x,&y);         sum=x+y;         printf("%d ",sum);     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-int main()
-{
-    int i,n,sum;
+int main(){
+    int n,i;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        int a,b;
-        scanf("%d%d",&a,&b);
-      printf("%d
-",a+b);
+    for(i=1;i<=n;i++){
+        int x,y,sum;
+        scanf("%d%d",&x,&y);
+        sum=x+y;
+        printf("%d
+",sum);
     }
 }
