5a0bb13b6750645454f9358db3646cb04271dc98
Keertana04 <119730054+Keertana04@users.noreply.github.com>
2023-05-23 08:42:24 +0530

    #include<stdio.h> int main(){         int n,i,j,x;         scanf("%d",&n);         x=65+n-1;         for(i=n;i>=1;i--){             for(j=1;j<=i;j++){                 printf("%c ",x);             }             printf(" ");             x=x-1;         }     }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,20 +1,14 @@
 #include<stdio.h>
 int main(){
-    int n,i,j,k;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=1;j<=n-i;j++){
-            printf(" ");
-        }
-        for(k=1;k<=n;k++){
-            if(i==1 || i==n || k==1|| k==n){
-                printf("*");
+        int n,i,j,x;
+        scanf("%d",&n);
+        x=65+n-1;
+        for(i=n;i>=1;i--){
+            for(j=1;j<=i;j++){
+                printf("%c ",x);
             }
-            else{
-                printf(" ");
-            }
-        }
-        printf("
+            printf("
 ");
+            x=x-1;
+        }
     }
-}
